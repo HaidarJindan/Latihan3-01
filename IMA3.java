a2609c4c7e761c0a855fea8f4b42d16c1b7f55bb
Haidar341 <91077398+Haidar341@users.noreply.github.com>
2021-10-06 14:00:34 +0700

    IMA3.java

---
+++ IMA3.java
@@ -0,0 +1,22 @@
+public class IMA3{
+    public static void main(String[] args){
+	     int penumpang = 0;
+		 penumpang = penumpang + 2;
+		 System.out.println("Order 1 = 2 Orang");
+		 penumpang = penumpang + 4;
+		 System.out.println("Order 2 = 4 Orang");
+		 penumpang = penumpang + 3;
+		 System.out.println("Order 3 = 3 Orang");
+		 penumpang = penumpang + 1;
+		 System.out.println("Order 4 = 1 Orang");
+		 penumpang = penumpang + 2;
+		 System.out.println("Order 5 = 2 Orang");
+		 penumpang = penumpang + 4;
+		 System.out.println("Order 6 = 4 Orang");
+		 penumpang = penumpang + 3;
+		 System.out.println("Order 7 = 3 Orang");
+		  
+		 System.out.println("Jumlah penumpang: " + penumpang);
+		 
+	}
+}	
